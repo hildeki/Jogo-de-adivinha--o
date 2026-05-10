52a73bd81f69cf666bf1ef68dea938cdb0b70d40
Hideki <lucashidekitogo0227@gmail.com>
2026-05-10 20:39:13 -0300

    Função iniciarJogo criada no JS

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const TOTAL_TENTATIVAS = 10;
   let jogoAtivo;
   let minimoAtual;
   let maximoAtual;
+ 
+  function iniciarJogo() {
+  numeroSecreto = Math.floor(Math.random() * 100) + 1;
+  tentativasRestantes = TOTAL_TENTATIVAS;
+  jogoAtivo = true;
+  minimoAtual = 1;
+  maximoAtual = 100;
+}
